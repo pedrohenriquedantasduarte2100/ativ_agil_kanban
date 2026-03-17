0a96c3dc568ebcb041c593ebb71e39d7a73244f7
PEDRO HENRIQUE DANTAS DUARTE <43049122838@info.local>
2026-03-17 19:54:18 -0300

    teste validar

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-from processos import melhor_aluno, processar_alunos
-
-
+from processo import validar_notas
 
 alunos = [
     ("Ana", [8, 7.5, 9]),
@@ -9,38 +7,10 @@ alunos = [
     ("Diana", []),
     ("Eduardo", [7, "a", 8])
     ]
-
-resultados = processar_alunos(alunos)
-
-    
-with open("resultados.txt", "w", encoding="utf-8") as arquivo:
-
-    for nome, notas, media, situacao in resultados:
-        linha = (
-            f"Aluno: {nome}\n"
-            f"Notas: {notas}\n"
-            f"Média: {media}\n"
-            f"Situação: {situacao}\n"
-            + "-" * 30 + "\n"
-            )
-
-        print(linha)          
-        arquivo.write(linha)  
-
-        
-    melhor = melhor_aluno(resultados)
-
-    if melhor:
-        nome, notas, media, situacao = melhor
-        texto = (
-            "\nMelhor aluno:\n"
-            f"Nome: {nome}\n"
-            f"Média: {media}\n"
-            )
+for aluno in alunos:
+    nome, notas = aluno
+    print(f"Validando notas de {nome}...")
+    if validar_notas(notas):
+        print(f"As notas de {nome} são válidas.")
     else:
-        texto = "\nNenhum aluno válido encontrado.\n"
-
-    print(texto)
-    arquivo.write(texto)
-
-
+        print(f"As notas de {nome} são inválidas.")
--- processo.py
+++ processo.py
@@ -1,1 +1,9 @@
-print ("ola")
+def validar_notas(notas):
+    if not isinstance(notas, list) or len(notas) == 0:
+        return False
+
+    for n in notas:
+        if not isinstance(n, (int, float)):
+            return False
+
+    return True
