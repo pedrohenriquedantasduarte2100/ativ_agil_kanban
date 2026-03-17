a28798f3419632b89961cdb3f9ae65cce6c0931c
PEDRO HENRIQUE DANTAS DUARTE <43049122838@info.local>
2026-03-17 20:01:15 -0300

    media junto com validar

--- processo.py
+++ processo.py
@@ -1,1 +1,19 @@
-print ("ola")
+def validar_notas(notas):
+    if not isinstance(notas, list) or len(notas) == 0:
+        return False
+
+    for n in notas:
+        if not isinstance(n, (int, float)):
+            return False
+
+    return True
+
+def calcular_media(notas):
+    if not validar_notas(notas):
+        return None
+
+    soma = 0
+    for n in notas:
+        soma += n
+
+    return soma / len(notas)
