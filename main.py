00f338a86d8ba7e261646adabc26e25676c58329
PEDRO HENRIQUE DANTAS DUARTE <43049122838@info.local>
2026-03-17 20:09:43 -0300

    todas as funçoes e teste

--- main.py
+++ main.py
@@ -1,3 +1,15 @@
+from processo import melhor_aluno, processar_alunos
 
 
 
+alunos = [
+    ("Ana", [8, 7.5, 9]),
+    ("Bruno", [5, 6, 6.5]),
+    ("Carlos", [10, 9, 8]),
+    ("Diana", []),
+    ("Eduardo", [7, "a", 8])
+    ]
+resultados = processar_alunos(alunos)
+melhor = melhor_aluno(resultados)
+if melhor:
+    print(f"O melhor aluno é {melhor[0]} com média {melhor[2]:.2f}")
